fb4d0370e965d0fa016e4b0387e211839e4cb8d0
joeljdv <joeldiazjdv@gmail.com>
2021-03-19 13:45:21 -0400

    commit tree 842aa7eca140c2b6d9661666f814a82112f7c31e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names=['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names,event){
+    const answers=[]
+    for( let i=0;i<names.length;i++){
+        answers.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        }
+    return answers
+    }
+
+    
+function countDown(num){
+     while(num>=0){
+         console.log(num--)
+     }
+}
